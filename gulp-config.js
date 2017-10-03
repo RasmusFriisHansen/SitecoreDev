1d3e20c81bd8e0a3bb9671655c255fd49e956136
Rasmus Friis Hansen <rfh@alpha-solutions.dk>
2017-10-03 15:21:23 +0200

    Chapter 4, page 167 Added gulpfiles to project

---
+++ gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\sitecore8\\Website",
+        devRoot: "C:\\Projects\\SitecoreDev",
+        solutionName: "SitecoreDev",
+        buildConfiguration: "Debug"
+    }
+    return config; 
+}
