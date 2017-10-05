7791b47721d27bfd019372e4d78f27587c6ac14b
Rasmus Friis Hansen <rfh@alpha-solutions.dk>
2017-10-05 12:25:30 +0200

    Sitecore8, page 188 Article with comments

---
+++ Feature/Articles/SitecoreDev.Feature.Articles/Repositories/ICommentRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using SitecoreDev.Feature.Articles.Models;
+namespace SitecoreDev.Feature.Articles.Repositories
+{
+  public interface ICommentRepository
+  {
+    IEnumerable<IComment> GetComments(string blogId);
+  }
+}
---
+++ Feature/Articles/SitecoreDev.Feature.Articles/Services/IContentService.cs
@@ -0,0 +1,8 @@
+﻿using SitecoreDev.Feature.Articles.Models;
+namespace SitecoreDev.Feature.Articles.Services
+{
+  public interface IContentService
+  {
+    IArticle GetArticleContent(string contentGuid);
+  }
+}
